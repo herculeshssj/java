fbc6b49dd590cf85a3df494cbcbaeee317a5c52d
Hércules S. S. José <hercules_hssj@outlook.com>
2023-07-29 05:36:35 -0300

    Importado os arquivos do projeto.

---
+++ concurso/src/main/java/br/com/hslife/concurso/ConcursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.hslife.concurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcursoApplication.class, args);
+	}
+
+}
---
+++ concurso/src/test/java/br/com/hslife/concurso/ConcursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hslife.concurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
