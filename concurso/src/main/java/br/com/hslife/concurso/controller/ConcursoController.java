1c6130c95b146b77df83414fad661f83788201bc
Hércules S. S. José <hercules_hssj@outlook.com>
2023-08-01 19:51:54 -0300

    Implementado método para arquivar um concurso.

--- concurso/src/main/java/br/com/hslife/concurso/controller/ConcursoController.java
+++ concurso/src/main/java/br/com/hslife/concurso/controller/ConcursoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -109,6 +110,20 @@ public class ConcursoController {
             return new ResponseEntity<>(concursos, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/archive/{id}")
+    public ResponseEntity<Concurso> arquivarConcurso(@PathVariable("id") Long id) {
+        Optional<Concurso> concurso = concursoRepository.findById(id);
+
+        if (concurso.isPresent()) {
+            Concurso _concurso = concurso.get();
+            _concurso.setArquivado(true);
+            
+            return new ResponseEntity<>(concursoRepository.save(_concurso), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
