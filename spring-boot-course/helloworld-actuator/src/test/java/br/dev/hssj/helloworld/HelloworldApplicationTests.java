310af7b728090cce8b8fcfa165f94f7257bcb18e
Hércules S. S. José <hercules_hssj@outlook.com>
2024-01-04 21:41:41 -0300

    Spring Actuator

---
+++ spring-boot-course/helloworld-actuator/src/test/java/br/dev/hssj/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.hssj.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
