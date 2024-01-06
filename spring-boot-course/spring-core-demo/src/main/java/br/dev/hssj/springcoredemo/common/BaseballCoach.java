bf5926c5dfe5ab91cae26a97268b0f764f63be23
Hércules S. S. José <hercules_hssj@outlook.com>
2024-01-06 13:33:54 -0300

    Qualifier example.

---
+++ spring-boot-course/spring-core-demo/src/main/java/br/dev/hssj/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,11 @@
+package br.dev.hssj.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes in batting practice";
+    }
+}
--- spring-boot-course/spring-core-demo/src/main/java/br/dev/hssj/springcoredemo/rest/DemoController.java
+++ spring-boot-course/spring-core-demo/src/main/java/br/dev/hssj/springcoredemo/rest/DemoController.java
@@ -3,6 +3,7 @@ package br.dev.hssj.springcoredemo.rest;
 import br.dev.hssj.springcoredemo.common.Coach;
 import br.dev.hssj.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @Autowired
-    private Util util;
+    private Util util; // this way of injection is not recommended by Spring Team
+    // This way make the code harder to unit test
     private Coach myCoach;
 
+    /*
     // dependency injection by constructor
     @Autowired
     public DemoController(Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+    */
+
+    // dependency injection by setter method
+    @Autowired
+    public void setMyCoach(@Qualifier("tennisCoach") Coach theCoach) {
         this.myCoach = theCoach;
     }
 
